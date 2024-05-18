ed4d9bff90ef58e132468ff4acbfc21d33e9a733
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-18 20:51:26 -0300

    adicionando a função de excluir

--- api/booksRoutes.js
+++ api/booksRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { getConnection, oracledb } = require('../database/oracleConnection')
-const { existsOrError } = require('./validations');
-
+const { existsOrError } = require('./validations')
 
 router.get('/books', async (req, res) => {
 	let connection
@@ -28,44 +27,71 @@ router.get('/books', async (req, res) => {
 })
 
 router.post('/books', async (req, res) => {
-    const {title, author, pages, thumbnail } = req.body //vai vir do front esses dados ou do postman para teste
+	const { title, author, pages, thumbnail } = req.body //vai vir do front esses dados ou do postman para teste
 
-    try {
-        existsOrError(title, 'O Titulo não foi nformado')
-        existsOrError(author, 'O Titulo não foi nformado')
-        existsOrError(pages, 'O Titulo não foi nformado')
-    } catch (msg) {
-        return res.status(400).send(msg)
-    }
+	try {
+		existsOrError(title, 'O Titulo não foi nformado')
+		existsOrError(author, 'O Titulo não foi nformado')
+		existsOrError(pages, 'O Titulo não foi nformado')
+	} catch (msg) {
+		return res.status(400).send(msg)
+	}
 
-    let connection
-    try {
-        connection = await getConnection();
-    
-        const result = await connection.execute(
-          `INSERT INTO books (TITLE, AUTHOR, PAGES, THUMBNAIL) VALUES (:title, :author, :pages, :thumbnail) RETURNING ID INTO :id`,
-          {
-            title,
-            author,
-            pages,
-            thumbnail,
-            id: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT }
-          }
-        );
-    
-        await connection.commit();
-    
-        // Retorne o livro recém-adicionado com o ID gerado
-        const newBook = {
-          ID: result.outBinds.id[0],
-          TITLE: title,
-          AUTHOR: author,
-          PAGES: pages,
-          THUMBNAIL: thumbnail
-        };
-    
-        res.status(201).json(newBook);
-    } catch (err) {
+	let connection
+	try {
+		connection = await getConnection()
+
+		const result = await connection.execute(
+			`INSERT INTO books (TITLE, AUTHOR, PAGES, THUMBNAIL) VALUES (:title, :author, :pages, :thumbnail) RETURNING ID INTO :id`,
+			{
+				title,
+				author,
+				pages,
+				thumbnail,
+				id: { type: oracledb.NUMBER, dir: oracledb.BIND_OUT },
+			}
+		)
+
+		await connection.commit()
+
+		// Retorne o livro recém-adicionado com o ID gerado
+		const newBook = {
+			ID: result.outBinds.id[0],
+			TITLE: title,
+			AUTHOR: author,
+			PAGES: pages,
+			THUMBNAIL: thumbnail,
+		}
+
+		res.status(201).json(newBook)
+	} catch (err) {
+		console.error(err)
+		res.status(500).json({ error: err.message })
+	} finally {
+		if (connection) {
+			await connection.close()
+		}
+	}
+})
+
+router.delete('/books/:id', async (req, res) => {
+	const id = req.params.id //pegar o id da requisição
+  console.log(id)
+
+	let connection
+
+	try {
+		connection = await getConnection() //cria a conexão com o banco de dados
+
+		const result = await connection.execute(`DELETE FROM BOOKS WHERE id = :1`, [id])
+
+		if (result.rowsAffected === 0) { // se tiver alguma linha que não foi afetada
+			res.status(404).send('Registro não encontrado') //então registro não encontrado
+		} else {
+			await connection.commit() // faço o commit no db
+			res.status(200).send('Registro removido com sucesso') //envio a resposta para o frontend
+		}
+	} catch (err) {
 		console.error(err)
 		res.status(500).json({ error: err.message })
 	} finally {
