e4cfebac3189651ea8b10e39313944b6ac8f799c
Eduardo Lange <eduardoandrelange@gmail.com>
2024-05-30 15:28:26 -0300

    ok

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken');
 const cors = require('cors')
 const usersRoutes = require('./api/usersRoutes')
 const booksRoutes = require('./api/booksRoutes')
